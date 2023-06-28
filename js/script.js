9a0a5e1e1f0f0d6d3dc0d81f24b7f32d71547655
NikoleGarc <nikoleupc@gmail.com>
2023-06-28 15:46:14 -0500

    func pagina cartera
    Funcionalidad Registrar tarjeta terminada
    Funcionalidad visualizar todas las tarjetas terminada

--- js/script.js
+++ js/script.js
@@ -1,1 +1,62 @@
-const tarjetaForm = document.createElement
+const cardsForm = document.getElementById('cardsForm');
+const nombreBancoInput = document.getElementById('nombreBanco');
+const numeroCuenta = document.getElementById('numeroCuenta');
+const saldoInput = document.getElementById('saldo');
+const cardsTableBody = document.getElementById('cardsTableBody');
+
+window.addEventListener('DOMContentLoaded', () => {
+    getCardsList();
+  });
+  
+  cardsForm.addEventListener('submit', e => {
+    e.preventDefault();
+  
+    const nombreBanco = nombreBancoInput.value;
+    const saldo = saldoInput.value;
+  
+    const tarjeta = {
+      nombre: nombreBanco,
+      saldo: saldo
+    };
+  
+    fetch('http://localhost:3000/tarjetas', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(tarjeta)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('tarjeta registrada:', data);
+        cardsForm.reset();
+        getCardsList();
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
+  
+const getCardsList = () => {
+    fetch('http://localhost:3000/tarjetas')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Lista de tarjetas:', data);
+        updateTarjetaTable(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+  const updateTarjetaTable = async (tarjetas) => {
+    cardsTableBody.innerHTML = '';
+  
+    for (const tarjeta of tarjetas) {  
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${tarjeta.nombre}</td>
+        <td>${tarjeta.saldo}</td>
+      `;
+      cardsTableBody.appendChild(row);
+    }
+  };
